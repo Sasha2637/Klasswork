530452db433333eca65adf3370d08880e2a2bdee
Sasha2637 <163992599+Sasha2637@users.noreply.github.com>
2024-03-21 18:36:45 +0300

    20.02.2024 Практическая работа №2

---
+++ main (1).py
@@ -0,0 +1,28 @@
+#coding: utf-8
+#Задача № 1
+x = int(input("Введите x ="))
+y = 7 * x + 5
+print("y = ", y)
+
+#Задача № 2
+a = int(input("Введите a = "))
+p = a * 4
+print("P = ", P)
+
+#Задача № 3
+r = int(input("Введите r = "))
+P = 3.14
+L = 2 * P * r
+print("L = ", L)
+
+#Задача № 4
+d = int(input("Введите d = "))
+P = 3.14
+S = (P * d ** 2) / 4
+print("S = ", S)
+
+#Задача № 5
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+c = √ (a ** 2 + b **2)
+print("c = ", c)
