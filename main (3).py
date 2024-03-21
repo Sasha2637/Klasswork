65b249642c51c8c5eb547a7cd75bd1459dc6f1a5
Sasha2637 <163992599+Sasha2637@users.noreply.github.com>
2024-03-21 18:49:47 +0300

    27.02.2024 Практическая работа №4

---
+++ main (3).py
@@ -0,0 +1,40 @@
+#coding: utf-8
+#Задача № 11
+V = int(input("Введите V = "))
+m = int(input("Введите m = "))
+P = m / V
+print("P = ", P)
+
+#Задача № 12
+x = int(input("Введите x = "))
+y = int(input("Введите y = "))
+z = x + y
+w = x - y
+n = x * y
+i = x / y
+print("z = ", z)
+print("w = ", w)
+print("n = ", m)
+print("i = ", i)
+
+#Задача № 13
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+j = (a + b) / 2
+g = 2 √ (a * b)
+print("j = ", j)
+print("g = ", g)
+#Задача № 14
+d = int(input("Введите d = "))
+f = int(input("Введите f = "))
+e = d / f
+print("e = ", e)
+
+#Задача № 15
+r = int(input("Введите r = "))
+k = int(input("Введите k = "))
+h = int(input("Введите h = "))
+u =  2 (a * b + b * c + a * c) 
+q = r * k * h 
+print("u = ", u)
+print("q = ", q)
