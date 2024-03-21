f378009efd9f9d020c2c34c0fcd7293b776a4a76
Sasha2637 <163992599+Sasha2637@users.noreply.github.com>
2024-03-21 18:10:19 +0300

    20.02.2024 Практическая работа №1

---
+++ main.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+#Задача № 1
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+print(tseloe, drobnoe, stroka)
+
+#Задача № 2
+big_netseloe = tseloe * 3.5
+print(big_netseloe)
+
+#Задача № 3
+drobnoe = drobnoe - 1
+print(drobnoe)
+
+#Задача № 4
+print(tseloe/drobnoe)
+print(big_netseloe/drobnoe)
+
+#Задача № 5
+stroka = stroka + stroka + "_" + "Yes" *3
+print(stroka)
